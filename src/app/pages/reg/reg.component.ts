ef93a48c07010c1b364125cf362b79e93929871f
Kanyó József <kanyojozsef96@gmail.com>
2021-04-16 16:07:07 +0200

    registration form style

--- src/app/pages/reg/reg.component.ts
+++ src/app/pages/reg/reg.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reg',
   templateUrl: './reg.component.html',
   styleUrls: ['./reg.component.scss']
 })
-export class RegComponent implements OnInit {
+export class RegComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.minLength(8),
+      Validators.required, Validators.pattern('^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required]),
+    passwordAgain: new FormControl('', [Validators.minLength(6), Validators.required]),
+  });
 
-  constructor() { }
+  error = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router) { }
+
+  // TODO: firebase Auth
+  registration(): void {
+    this.error = false;
+    if (this.form.valid) {
+      if (this.form.value.password === this.form.value.passwordAgain) {
+        console.log(this.form.value);
+        // this.router.navigateByUrl('/login');
+        return;
+      }
+    }
+    this.error = true;
   }
-
 }
--- src/app/pages/reg/reg.module.ts
+++ src/app/pages/reg/reg.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegComponent } from './reg.component';
 import { RegRoutingModule } from './reg-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 
@@ -11,7 +17,14 @@ import { RegRoutingModule } from './reg-routing.module';
   ],
   imports: [
     CommonModule,
-    RegRoutingModule
+    RegRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule
   ],
   exports: [
     RegComponent
